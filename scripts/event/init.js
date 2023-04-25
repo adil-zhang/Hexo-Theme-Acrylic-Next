632d016e601242c952d8d4c97593ec0ea76c56ee
Adil <adil_zhang@163.com>
2023-04-25 09:51:38 +0800

    🎈 perf: 添加报错内容

--- scripts/event/init.js
+++ scripts/event/init.js
@@ -1,7 +1,7 @@
 hexo.extend.filter.register('before_generate', () => {
     // Get first two digits of the Hexo version number
     const hexoVer = hexo.version.replace(/(^.*\..*)\..*/, '$1')
-    const logger = hexo.log, config = hexo.config
+    const logger = hexo.log, config = hexo.config, theme = hexo.theme.config
   
     if (hexoVer < 5.3) {
       logger.error('请把 Hexo 升级到 V5.3.0 或更高的版本！')
@@ -11,5 +11,9 @@ hexo.extend.filter.register('before_generate', () => {
     if(config.prismjs.enable){
       logger.error('主题尚未支持 prismjs 请使用 highlightjs ！')
       process.exit(-1)
+    }
+    if (theme.hometop.banner && !theme.hometop.icon) {
+      logger.error('\n 启用banner的情况下，必须提供 icon 图片！\n 请在主题配置文件中设置 hometop.banner.icon 选项。');
+      process.exit(-1);
     }
   })

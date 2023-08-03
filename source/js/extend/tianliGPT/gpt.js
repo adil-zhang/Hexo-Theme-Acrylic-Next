05b53a55b2f855fd1b2cd085a3a1bf5c2908aeb1
Adil <adil_zhang@163.com>
2023-08-03 14:44:22 +0800

    ✨ feat: GPT更新

--- source/js/extend/tianliGPT/gpt.js
+++ source/js/extend/tianliGPT/gpt.js
@@ -1,6 +1,8 @@
 
 let tianliGPT_postSelector = '\#post \#article-container';
 let tianliGPT_key = '5lrPe5FvVDCkjRlIBlZg'
+var tianliGPTIsRunning = false;
+
 function insertAIDiv(selector) {
   // 首先移除现有的 "post-TianliGPT" 类元素（如果有的话）
   removeExistingAIDiv();
@@ -31,8 +33,7 @@ function insertAIDiv(selector) {
   <g id="&#x673A;&#x5668;&#x4EBA;" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
     <path d="M34.717885,5.03561087 C36.12744,5.27055371 37.079755,6.60373651 36.84481,8.0132786 L35.7944,14.3153359 L38.375,14.3153359 C43.138415,14.3153359 47,18.1768855 47,22.9402569 L47,34.4401516 C47,39.203523 43.138415,43.0650727 38.375,43.0650727 L9.625,43.0650727 C4.861585,43.0650727 1,39.203523 1,34.4401516 L1,22.9402569 C1,18.1768855 4.861585,14.3153359 9.625,14.3153359 L12.2056,14.3153359 L11.15519,8.0132786 C10.920245,6.60373651 11.87256,5.27055371 13.282115,5.03561087 C14.69167,4.80066802 16.024865,5.7529743 16.25981,7.16251639 L17.40981,14.0624532 C17.423955,14.1470924 17.43373,14.2315017 17.43948,14.3153359 L30.56052,14.3153359 C30.56627,14.2313867 30.576045,14.1470924 30.59019,14.0624532 L31.74019,7.16251639 C31.975135,5.7529743 33.30833,4.80066802 34.717885,5.03561087 Z M38.375,19.4902885 L9.625,19.4902885 C7.719565,19.4902885 6.175,21.0348394 6.175,22.9402569 L6.175,34.4401516 C6.175,36.3455692 7.719565,37.89012 9.625,37.89012 L38.375,37.89012 C40.280435,37.89012 41.825,36.3455692 41.825,34.4401516 L41.825,22.9402569 C41.825,21.0348394 40.280435,19.4902885 38.375,19.4902885 Z M14.8575,23.802749 C16.28649,23.802749 17.445,24.9612484 17.445,26.3902253 L17.445,28.6902043 C17.445,30.1191812 16.28649,31.2776806 14.8575,31.2776806 C13.42851,31.2776806 12.27,30.1191812 12.27,28.6902043 L12.27,26.3902253 C12.27,24.9612484 13.42851,23.802749 14.8575,23.802749 Z M33.1425,23.802749 C34.57149,23.802749 35.73,24.9612484 35.73,26.3902253 L35.73,28.6902043 C35.73,30.1191812 34.57149,31.2776806 33.1425,31.2776806 C31.71351,31.2776806 30.555,30.1191812 30.555,28.6902043 L30.555,26.3902253 C30.555,24.9612484 31.71351,23.802749 33.1425,23.802749 Z" id="&#x5F62;&#x72B6;&#x7ED3;&#x5408;" fill="#444444" fill-rule="nonzero"></path>
   </g>
-  </svg>`
-
+  </svg>`;
   const aiTitleTextDiv = document.createElement('div');
   aiTitleTextDiv.className = 'tianliGPT-title-text';
   aiTitleTextDiv.textContent = 'AI摘要';
@@ -41,7 +42,7 @@ function insertAIDiv(selector) {
   const aiTagDiv = document.createElement('div');
   aiTagDiv.className = 'tianliGPT-tag';
   aiTagDiv.id = 'tianliGPT-tag';
-  aiTagDiv.textContent = 'TianliGPT';
+  aiTagDiv.textContent = '智能摘要';
   aiTitleDiv.appendChild(aiTagDiv);
 
   const aiExplanationDiv = document.createElement('div');
@@ -70,7 +71,6 @@ var tianliGPT = {
       const title = document.title;
       const container = document.querySelector(tianliGPT_postSelector);
       if (!container) {
-        console.warn('TianliGPT：找不到文章容器。请尝试将引入的代码放入到文章容器之后。如果本身没有打算使用摘要功能可以忽略此提示。');
         return '';
       }
       const paragraphs = container.getElementsByTagName('p');
@@ -108,8 +108,9 @@ var tianliGPT = {
     if (tianliGPT_key === "5Q5mpqRK5DkwT1X9Gi5e") {
       return "请购买 key 使用，如果你能看到此条内容，则说明代码安装正确。";
     }
-
-    const apiUrl = `https://summary.tianli0.top/?content=${encodeURIComponent(content)}&key=${encodeURIComponent(tianliGPT_key)}`;
+    var url = window.location.href;
+    const title = document.title;
+    const apiUrl = `https://summary.tianli0.top/?content=${encodeURIComponent(content)}&key=${encodeURIComponent(tianliGPT_key)}&url=${encodeURIComponent(url)}&title=${encodeURIComponent(title)}`;
     const timeout = 20000; // 设置超时时间（毫秒）
   
     try {
@@ -120,7 +121,12 @@ var tianliGPT = {
             const data = await response.json();
             return data.summary;
         } else {
-            throw new Error('请求失败');
+            if (response.status === 402) {
+                document.querySelectorAll('.post-TianliGPT').forEach(el => {
+                    el.style.display = 'none';
+                });
+            }
+            throw new Error('TianliGPT：余额不足，请充值后请求新的文章');
         }
     } catch (error) {
         if (error.name === 'AbortError') {
@@ -136,18 +142,80 @@ var tianliGPT = {
             return '获取文章摘要失败，请稍后再试。';
         }
     }
-  }
+  },
+
+  aiShowAnimation: function (text) {
+    const element = document.querySelector(".tianliGPT-explanation");
+    if (!element) {
+      return;
+    }
+
+    if (tianliGPTIsRunning) {
+      return;
+    }
+    tianliGPTIsRunning = true;
+    const typingDelay = 25;
+    const waitingTime = 1000;
+    const punctuationDelayMultiplier = 6;
+
+    element.style.display = "block";
+    element.innerHTML = "生成中..." + '<span class="blinking-cursor"></span>';
+
+    let animationRunning = true;
+    let currentIndex = 0;
+    let initialAnimation = true;
+    let lastUpdateTime = performance.now();
+
+    const animate = () => {
+      if (currentIndex < text.length && animationRunning) {
+        const currentTime = performance.now();
+        const timeDiff = currentTime - lastUpdateTime;
+
+        const letter = text.slice(currentIndex, currentIndex + 1);
+        const isPunctuation = /[，。！、？,.!?]/.test(letter);
+        const delay = isPunctuation ? typingDelay * punctuationDelayMultiplier : typingDelay;
+
+        if (timeDiff >= delay) {
+          element.innerText = text.slice(0, currentIndex + 1);
+          lastUpdateTime = currentTime;
+          currentIndex++;
+
+          if (currentIndex < text.length) {
+            element.innerHTML =
+              text.slice(0, currentIndex) +
+              '<span class="blinking-cursor"></span>';
+          } else {
+            element.innerHTML = text;
+            element.style.display = "block";
+            tianliGPTIsRunning = false;
+            observer.disconnect();// 暂停监听
+          }
+        }
+        requestAnimationFrame(animate);
+      }
+    }
+
+    // 使用IntersectionObserver对象优化ai离开视口后暂停的业务逻辑，提高性能
+    const observer = new IntersectionObserver((entries) => {
+      let isVisible = entries[0].isIntersecting;
+      animationRunning = isVisible; // 标志变量更新
+      if (animationRunning && initialAnimation) {
+        setTimeout(() => {
+          requestAnimationFrame(animate);
+        }, 200);
+      }
+    }, { threshold: 0 });
+    let post_ai = document.querySelector('.post-TianliGPT');
+    observer.observe(post_ai);//启动新监听
+
+  },
 }
 
 function runTianliGPT() {
   insertAIDiv(tianliGPT_postSelector);
   const content = tianliGPT.getTitleAndContent();
-  if (!content && content !== '') {
-    console.log('TianliGPT本次提交的内容为：' + content);
-  }
   tianliGPT.fetchTianliGPT(content).then(summary => {
-    const aiExplanationDiv = document.querySelector('.tianliGPT-explanation');
-    aiExplanationDiv.innerHTML = summary;
+    tianliGPT.aiShowAnimation(summary);
   })
 }
 
@@ -178,3 +246,7 @@ function checkURLAndRun() {
     console.error("TianliGPT：我没有看懂你编写的自定义链接规则，所以我决定不执行摘要功能", error);
   }
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+  checkURLAndRun();
+});
